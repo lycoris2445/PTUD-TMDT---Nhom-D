5dd6e842a732522330b0b24abfb30daad0c404c2
Nguyen Anh Thy <thynguyen.31231022228@st.ueh.edu.vn>
2025-12-19 21:40:28 +0700

    Lưu code của Anh Thy vào nhánh mới

--- website/php/policy.php
+++ website/php/policy.php
@@ -6,7 +6,8 @@ include 'header.php';
 
 <main class="container py-5">
   <h2 class="section-title">Legal & Privacy</h2>
-  <?php include 'policy-content.php'; ?>
+  
+
 </main>
 
 <?php include 'footer.php'; ?>
